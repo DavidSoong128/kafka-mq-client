015a01b9710e8d8d5bdbc65e70bff734c7959d71
dabao <dabao@juanpi.com>
2016-01-04 14:05:18 +0800

    build project , the target is become best kafka client connection project

---
+++ src/main/java/com.horizon.kafka.client/utils/StringUtils.java
@@ -0,0 +1,11 @@
+package com.horizon.kafka.client.utils;
+
+/**
+ * the tool for handling String data
+ * @author : David.Song/Java Engineer
+ * @date : 2016/1/4 14:03
+ * @see
+ * @since : 1.0.0
+ */
+public class StringUtils {
+}
